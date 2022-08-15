b68d5623f09b4049baa498434dad314c7a0a36df
Lucas Macedo <lucas.macedo@gsuite.iff.edu.br>
2022-08-15 00:55:05 -0300

    versao 08

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var lista=[];
 function marcarprato(numero,caminho,item){
     const elemento=document.querySelector(caminho);
     if (elemento!==null){
@@ -11,16 +12,31 @@ function marcarprato(numero,caminho,item){
     teste(item);
 }
 function teste(item){
+    lista=[];
     if (document.querySelector('.prato-selecionadof') !== null && document.querySelector('.bebida-selecionadof') !== null && document.querySelector('.sobremesa-selecionadof')!==null){
         const botao=document.querySelector('.selec-fechar');
         botao.classList.add('verde');
         botao.querySelector('.pinit').classList.add('desaparecer');
         botao.querySelector('.pfin').classList.remove('desaparecer');
         botao.setAttribute('onclick','cancelar()');
-        let oi4=Number(document.querySelector('.prato-selecionadof p').innerText.slice(3,).replace(',','.'))+Number(document.querySelector('.bebida-selecionadof p').innerText.slice(3,).replace(',','.'))+Number(document.querySelector('.sobremesa-selecionadof p').innerText.slice(3,).replace(',','.'));
-        console.log(oi4);
-        oi4=oi4.toFixed(2).replace('.',',');
-        document.querySelector('.valor-total').innerHTML='R$ '+oi4;
+        let item1=document.querySelector('.prato-selecionadof h5').innerText;
+        let item2=document.querySelector('.bebida-selecionadof h5').innerText;
+        let item3=document.querySelector('.sobremesa-selecionadof h5').innerText;
+        let valor1=document.querySelector('.prato-selecionadof p').innerText.slice(3,);
+        let valor2=document.querySelector('.bebida-selecionadof p').innerText.slice(3,);
+        let valor3=document.querySelector('.sobremesa-selecionadof p').innerText.slice(3,);
+        let total=Number(valor1.replace(',','.'))+Number(valor2.replace(',','.'))+Number(valor3.replace(',','.'));
+        total=total.toFixed(2);
+        lista.push(item1,item2,item3,total);
+        console.log(lista);
+        total=total.replace('.',',');
+        document.querySelector('.item1').innerHTML=item1;
+        document.querySelector('.item2').innerHTML=item2;
+        document.querySelector('.item3').innerHTML=item3;
+        document.querySelector('.valor1').innerHTML=valor1;
+        document.querySelector('.valor2').innerHTML=valor2;
+        document.querySelector('.valor3').innerHTML=valor3;
+        document.querySelector('.valor-total').innerHTML='R$ '+total;
     }
 }
 function cancelar(){
@@ -28,4 +44,18 @@ function cancelar(){
     menu.classList.toggle('desaparecer');
     menu.classList.toggle('flex');
 }
+function fechar(){
+    const nome = prompt('Digite seu nome');
+    const endereco = prompt('Digete seu endereço');
+    let string = `Olá, gostaria de fazer o pedido:
+    - Prato: ${lista[0]}
+    - Bebida: ${lista[1]}
+    - Sobremesa: ${lista[2]}
+    Total: R$ ${lista[3]}
+    
+    Nome: ${nome}
+    Endereço: ${endereco}`
+    window.open('https://wa.me/5522981737527?text='+encodeURIComponent(string));
+}
+
 /*let oi4=oi.innerText.slice(4,).replace(',','.');*/
